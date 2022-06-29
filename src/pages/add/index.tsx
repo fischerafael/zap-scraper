42c1d54900ea64b9316bdd64e027934e3f0e2219
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-06-29 20:24:12 -0300

    feat: saving the link via the frontend

--- src/pages/add/index.tsx
+++ src/pages/add/index.tsx
@@ -1,11 +1,29 @@
 import { Button, Input, VStack } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export const PageAdd = () => {
+  const [zapLink, setZapLink] = useState("");
+
+  console.log(zapLink);
+
+  const handleScrape = async () => {
+    await axios
+      .get(`/api/zap?url=${zapLink}`)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <VStack w="full" bg="gray.900" color="white" minH="100vh" p="8">
+    <VStack w="full" bg="purple.900" color="white" minH="100vh" p="8">
       <VStack w="full" maxW="container.sm">
-        <Input w="full" placeholder="Link do imóvel" />
-        <Button w="full" colorScheme="cyan">
+        <Input
+          w="full"
+          placeholder="Link do imóvel"
+          value={zapLink}
+          onChange={(e) => setZapLink(e.target.value)}
+        />
+        <Button w="full" colorScheme="green" onClick={handleScrape}>
           Salvar
         </Button>
       </VStack>
