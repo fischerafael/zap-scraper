e44b3c5098fc23316434f87d34d2b24f53933125
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-06-29 19:44:09 -0300

    feat: working

---
+++ pages/api/zap.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { load } from "cheerio";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method, query } = req;
+
+  if (method === "GET") {
+    const { url } = query;
+    try {
+      if (!url) throw new Error("URL not provided");
+      const response = await fetch(url as string);
+      const htmlString = await response.text();
+      const $ = load(htmlString);
+      const price = $(
+        "div.info__base > div > div:nth-child(1) > ul > li > strong"
+      ).text();
+
+      res.status(200).json({ html: price, query });
+    } catch (e: any) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+
+  res.status(500).json({ error: "Method Not Allowed" });
+}
