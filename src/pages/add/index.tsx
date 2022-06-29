1a6a8b291ef2a728dc5724f0cf77c2644b2aabba
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-06-29 20:27:10 -0300

    feat: form working fine

--- src/pages/add/index.tsx
+++ src/pages/add/index.tsx
@@ -1,18 +1,32 @@
-import { Button, Input, VStack } from "@chakra-ui/react";
+import { Button, Input, Text, VStack } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const PageAdd = () => {
   const [zapLink, setZapLink] = useState("");
+  const [isDisabled, setDisabled] = useState(true);
+
+  const [result, setResult] = useState({ price: 0 });
 
   console.log(zapLink);
 
   const handleScrape = async () => {
     await axios
       .get(`/api/zap?url=${zapLink}`)
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        setResult({ ...result, price: res.data.price });
+        setZapLink("");
+      })
       .catch((err) => console.log(err));
   };
+
+  useEffect(() => {
+    if (zapLink) {
+      setDisabled(false);
+      return;
+    }
+    setDisabled(true);
+  }, [zapLink]);
 
   return (
     <VStack w="full" bg="purple.900" color="white" minH="100vh" p="8">
@@ -23,9 +37,18 @@ export const PageAdd = () => {
           value={zapLink}
           onChange={(e) => setZapLink(e.target.value)}
         />
-        <Button w="full" colorScheme="green" onClick={handleScrape}>
+        <Button
+          isDisabled={isDisabled}
+          w="full"
+          colorScheme="green"
+          onClick={handleScrape}
+        >
           Salvar
         </Button>
+      </VStack>
+
+      <VStack>
+        <Text>{result.price}</Text>
       </VStack>
     </VStack>
   );
